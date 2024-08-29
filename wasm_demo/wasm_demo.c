fbadbd41e2fb55e02f12f86ffb2f8b8844b31c1b
shoguncao <shoguncao@tencent.com>
2024-08-29 17:39:14 +0800

    增加日志打印。

--- wasm_demo/wasm_demo.c
+++ wasm_demo/wasm_demo.c
@@ -1,10 +1,12 @@
 #include <emscripten/emscripten.h>
+#include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
     
     int EMSCRIPTEN_KEEPALIVE csg_add_one(int a) {
+        printf("call from %s", __FILE__);
         return a + 1;
     }
     
